1f2e01853e59228db329414ef421f0ca0d8408e9
Maureenkamau03 <mkaymaureen@gmail.com>
2022-12-28 07:29:55 +0300

    added navbar links, css, shop and cart components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { ShoppingCart } from "phosphor-react";
+import "./navbar.css";
 function Navbar() {
-	return <div>Navbar</div>;
+	return (
+		<div className="nav">
+			<div className="links">
+				<Link to="/">Shop</Link>
+				<Link to="/cart">
+					<ShoppingCart size={32} />
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
